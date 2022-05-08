a8a669649b83c9112e5cd5bcb8cf8520f4a9c60d
Daniel K <code.danielk@gmail.com>
2022-05-08 11:37:27 +0200

    remove unused exec functions

--- src/i3util.h
+++ src/i3util.h
@@ -6,11 +6,6 @@
 
 #include <yajl/yajl_tree.h>
 
-typedef int (*wincheck_cb)(yajl_val obj, void *context);
-
-extern int exec_wait(int sock, const char *spath, char *command);
-extern int exec_wait_ex(int sock, const char *spath, char *command, long int timeout, wincheck_cb cb, void *context);
-
 extern int i3util_request_json(int sock, unsigned long type, char *data, i3_msg *msg, yajl_val *jobj);
 
 extern int i3util_subscribe(int sock, const char *data);
